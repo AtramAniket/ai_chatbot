c318d028ce6d2a065abc4050df6f5634b97d301e
AtramAniket <aniket_atram@live.in>
2026-01-21 14:43:43 +0530

    added user chat functionality

--- src/components/ChatBotApp.jsx
+++ src/components/ChatBotApp.jsx
@@ -1,48 +1,94 @@
-import './ChatBotApp.css'
+import { useState } from "react";
+import "./ChatBotApp.css";
 
-const ChatBotApp = ({onGoBack}) => {
+const ChatBotApp = ({ onGoBack, chats, setChats }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState(chats[0]?.messages || []);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const sendMessage = () => {
+    // create a new chat message object
+    const newMessage = {
+      type: "prompt",
+      text: inputValue,
+      timestamp: new Date().toLocaleTimeString(),
+    };
+
+    // update the message state with this new message
+    const updatedMessages = [...messages, newMessage];
+    setMessages(updatedMessages);
+    setInputValue("");
+
+    // the current message should be updated accordingly
+    const updateChats = chats.map((chat, index) => {
+      if (index === 0) {
+        return { ...chats, messages: updatedMessages };
+      }
+      return chat;
+    });
+
+    // update the chat state
+    setChats(updateChats);
+  };
+
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+			e.preventDefault()
+			sendMessage()
+		}
+	}
+
   return (
-    <div className='chat-app'>
-        <div className="chat-list">
-            <div className="chat-list-header">
-                <h2>Chat List</h2>
-                <i className="bx bx-edit-alt new-chat"></i>
-            </div>
-            <div className="chat-list-item active">
-                <h4>Chat 20/01/2026 12:24:33 PM</h4>
-                <i className="bx bx-x-circle"></i>
-            </div>
-            <div className="chat-list-item">
-                <h4>Chat 20/01/2026 12:24:33 PM</h4>
-                <i className="bx bx-x-circle"></i>
-            </div>
-            <div className="chat-list-item">
-                <h4>Chat 20/01/2026 12:24:33 PM</h4>
-                <i className="bx bx-x-circle"></i>
-            </div>
+    <div className="chat-app">
+      <div className="chat-list">
+        <div className="chat-list-header">
+          <h2>Chat List</h2>
+          <i className="bx bx-edit-alt new-chat"></i>
         </div>
-        <div className="chat-window">
-            <div className="chat-title">
-                <h3>Chat with AI</h3>
-                <i className="bx bx-arrow-back arrow" onClick={onGoBack}></i>
-            </div>
-            <div className="chat">
-                <div className="prompt">
-                    Hi, How are you <span>12:32:30 PM</span>
-                </div>
-                <div className="response">
-                    I am fine. Thank you for asking. How can I assist you today? <span>12:32:33 PM</span>
-                </div>
-                <div className="typing">Typing...</div>
-            </div>
-            <form className="msg-form">
-                <i className="fa-solid fa-face-smile emoji"></i>
-                <input type="text" className="msg-input" placeholder='Type a message...' />
-                <i className="fa-solid fa-paper-plane"></i>
-            </form>
+        {chats?.map((chat, index) => (
+          <div
+            key={index}
+            className={`chat-list-item ${index === 0 ? "active" : ""}`}
+          >
+            <h4>{chat?.id}</h4>
+            <i className="bx bx-x-circle"></i>
+          </div>
+        ))}
+      </div>
+      <div className="chat-window">
+        <div className="chat-title">
+          <h3>Chat with AI</h3>
+          <i className="bx bx-arrow-back arrow" onClick={onGoBack}></i>
         </div>
+        <div className="chat">
+          {messages?.map((msg, index) => (
+            <div
+              key={index}
+              className={`${msg?.type === "prompt" ? "prompt" : "response"}`}
+            >
+              {msg?.text} <span>{msg?.timestamp}</span>
+            </div>
+          ))}
+          <div className="typing">Typing...</div>
+        </div>
+        <form className="msg-form" onSubmit={(e) => e.preventDefault()}>
+          <i className="fa-solid fa-face-smile emoji"></i>
+          <input
+            type="text"
+            className="msg-input"
+            placeholder="Type a message..."
+            value={inputValue}
+            onChange={handleInputChange}
+						onKeyDown={handleKeyDown}
+          />
+          <i className="fa-solid fa-paper-plane" onClick={sendMessage}></i>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBotApp
+export default ChatBotApp;
